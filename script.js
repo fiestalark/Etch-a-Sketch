e39feb204da96563e682a19b4f2f23326224db57
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-11-19 10:08:48 -0500

    Add rainbow color and shading options

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const container = document.querySelector('.container');
-const btn = document.querySelector('button');
+const clear = document.querySelector('#clear');
+const changeSize = document.querySelector('#change-size');
+const rainbow = document.querySelector('#rainbow');
+const shade = document.querySelector('#shade');
 
-
-let startBlocks = 16;
+let rainbowOn = false;
+let shadeOn = false;
+let gridSize = 16;
 
 function drawGrid(blocks) {
     container.innerHTML = '';
@@ -15,20 +19,28 @@ function drawGrid(blocks) {
     }
 }
 
-drawGrid(startBlocks);
-etchASketch();
+function clearGrid() {
+    const childDivs = document.querySelectorAll('.sketch');
+
+    childDivs.forEach(div => {
+        let shade = div.dataset.shade;
+        let shadeClass = `shade-${shade}`;
+        div.classList.remove('etch', shadeClass);
+        div.style.removeProperty('--sketch-color');
+        delete div.dataset.shade});
+}
+
+drawGrid(gridSize);
 
 function getInput() {
-    let squares;
     do {
-        squares = prompt('How many squares per side?');
-        if (squares === null) {
+        gridSize = prompt('How many squares per side?');
+        if (gridSize === null) {
             return;
         }
-        squares = Number(squares);
-        if (Number.isInteger(squares) && squares <= 100 && squares > 1) {
-            drawGrid(squares);
-            etchASketch();
+        gridSize = Number(gridSize);
+        if (Number.isInteger(gridSize) && gridSize <= 100 && gridSize > 1) {
+            drawGrid(gridSize);
             return;
         }
         alert('Please enter a valid integer between 2 and 100.');
@@ -36,15 +48,49 @@ function getInput() {
 }
 
 
-btn.addEventListener('click', () => getInput());
+changeSize.addEventListener('click', () => getInput());
+rainbow.addEventListener('click', () => {
+    rainbowOn = !rainbowOn;
+    rainbow.classList.toggle('selected');
+});
+clear.addEventListener('click', clearGrid);
+shade.addEventListener('click', () => {
+    shadeOn = !shadeOn;
+    shade.classList.toggle('selected');
+});
 
-
-function etchASketch() {
-    const div = document.querySelector('.container');
-
-    div.addEventListener('mouseover', (e) => {
-        if (e.target.classList.contains('sketch')) {
-            e.target.classList.add('etch');
+container.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('sketch')) {
+        let color = rainbowOn ? randomColor() : 'black';
+        changeColor(color, e.target);
+        if (shadeOn) {
+            shadeBlock(e.target);
         }
-    })
+        e.target.classList.add('etch');
+    }
+})
+
+
+function shadeBlock(block) {
+    if (Number(block.dataset.shade) === 100) {
+        return;
+    } else if (block.dataset.shade) {
+        let currentShade = block.dataset.shade;
+        block.classList.remove(`shade-${currentShade}`);
+    
+        let newShade = Number(currentShade) + 10;
+        block.dataset.shade = newShade;
+        block.classList.add(`shade-${newShade}`)
+    } else {
+        block.dataset.shade = '10';
+        block.classList.add('shade-10');
+    }
+}
+
+function randomColor() {
+    return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+}
+
+function changeColor(color, e) {
+    e.style.setProperty('--sketch-color', color);
 }
